db1bb5d22dfd43ecb4958cc8543a13f98f12e23c
Lawrence Thomas <Lawrence@LawrencsMacBook.home>
2019-08-26 22:40:37 -0400

    Updated server

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import "./App.scss";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import setAuthToken from "./utils/setAuthToken";
 import jwt_decode from "jwt-decode";
+require("dotenv/config");
 
 import { setCurrentUser, logoutUser } from "./actions/authActions";
 import { Provider } from "react-redux";
