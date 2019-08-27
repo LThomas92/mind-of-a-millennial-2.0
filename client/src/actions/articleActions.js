3e4f7e7c9973b35ada483e11761c6541f580407c
Lawrence Thomas <Lawrence@LawrencsMacBook.home>
2019-08-26 22:56:10 -0400

    Updated server

--- client/src/actions/articleActions.js
+++ client/src/actions/articleActions.js
@@ -13,7 +13,7 @@ import AxiosAPI from "../components/AxiosAPI";
 // Add Article
 export const addArticle = articleData => dispatch => {
   dispatch(clearErrors());
-  AxiosAPI.post("api/articles/add", articleData)
+  AxiosAPI.post("/articles/add", articleData)
     .then(res =>
       dispatch({
         type: ADD_ARTICLE,
@@ -30,7 +30,7 @@ export const addArticle = articleData => dispatch => {
 //GET ALL ARTICLES
 export const getAllArticles = () => dispatch => {
   dispatch(setArticleLoading());
-  AxiosAPI.get("/api/articles")
+  AxiosAPI.get("/articles")
     .then(res =>
       dispatch({
         type: GET_ARTICLES,
@@ -47,7 +47,7 @@ export const getAllArticles = () => dispatch => {
 //GET ONE ARTICLE
 export const getArticle = slug => dispatch => {
   dispatch(setArticleLoading());
-  AxiosAPI.get(`/api/articles/show/${slug}`)
+  AxiosAPI.get(`/articles/show/${slug}`)
     .then(res =>
       dispatch({
         type: GET_ARTICLE,
@@ -64,7 +64,7 @@ export const getArticle = slug => dispatch => {
 
 //DELETE ARTICLE
 export const deleteArticle = id => dispatch => {
-  AxiosAPI.delete(`/api/articles/${id}`)
+  AxiosAPI.delete(`/articles/${id}`)
     .then(res =>
       dispatch({
         type: DELETE_ARTICLE,
