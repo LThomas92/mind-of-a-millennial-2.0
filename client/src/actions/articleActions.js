b6d3a7b2e55721d01210a1c88401e432265b6ddc
Lawrence Thomas <Lawrence@LawrencsMacBook.home>
2019-08-27 19:59:54 -0400

    Updated server

--- client/src/actions/articleActions.js
+++ client/src/actions/articleActions.js
@@ -14,7 +14,7 @@ import axios from "axios";
 export const addArticle = articleData => dispatch => {
   dispatch(clearErrors());
   axios
-    .post("/api/articles/add", articleData)
+    .post("/api/articles", articleData)
     .then(res =>
       dispatch({
         type: ADD_ARTICLE,
