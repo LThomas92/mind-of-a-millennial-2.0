ff50c693b023b700c0b8a350fbbd415b305e5d5e
Lawrence Thomas <Lawrence@LawrencsMacBook.home>
2019-08-28 14:05:29 -0400

    Updated server

--- client/src/components/articles/AllArticles.js
+++ client/src/components/articles/AllArticles.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { connect } from "react-redux";
-import { getArticles } from "../../actions/articleActions";
+import { getArticles } from "../../../src/actions/articleActions";
 
 class AllArticles extends React.Component {
   componentDidMount() {
