a054a019139d89016b6bae0add8f9ed72891103d
Lawrence Thomas <Lawrence@LawrencsMacBook.home>
2019-08-28 18:27:10 -0400

    Updated server

---
+++ client/build/precache-manifest.6b49d66860ca33924638a0552192fb38.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "78310b9222819fd10c97b033ee3832fb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "be931a193055efd662c1",
+    "url": "/static/css/2.4b3ccb6c.chunk.css"
+  },
+  {
+    "revision": "371b08d1f1b8538cdf2d",
+    "url": "/static/css/main.cb594193.chunk.css"
+  },
+  {
+    "revision": "be931a193055efd662c1",
+    "url": "/static/js/2.2b48af85.chunk.js"
+  },
+  {
+    "revision": "371b08d1f1b8538cdf2d",
+    "url": "/static/js/main.531bca9c.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "3f1312d4a293d08c1e2bffae546e0815",
+    "url": "/static/media/logo.3f1312d4.svg"
+  }
+]);
