62524506017aadeff5f01944093a27ab78a368da
Lawrence Thomas <Lawrence@LawrencsMacBook.home>
2019-08-28 19:03:00 -0400

    Updated server

--- client/src/components/AxiosAPI.js
+++ client/src/components/AxiosAPI.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export default axios.create({
   baseURL:
-    "http://mindofmillennial.com" ||
+    "https://mindofmillennial.com" ||
     "https://boiling-beyond-67498.herokuapp.com" ||
     "https://localhost:5000",
   responseType: "json"
