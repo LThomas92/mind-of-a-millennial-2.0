86bd932e4648e263fe0ef1e572f6bba2c8d51161
Lawrence Thomas <Lawrence@LawrencsMacBook.home>
2019-08-28 20:50:24 -0400

    Updated server

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ const DB = require("./config/keys").mongoURI;
 
 //connect to the database
 mongoose
-  .connect(DB, { useNewUrlParser: true })
+  .connect(process.env.mongoDB_URI, { useNewUrlParser: true })
   .then(() => console.log("Database connected successfully"))
   .catch(err => console.log(err));
 
