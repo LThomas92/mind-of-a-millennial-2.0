012c2e1dd3cf5dad70ccb8fdbfc4048d298ced72
Lawrence Thomas <Lawrence@LawrencsMacBook.home>
2019-08-28 21:31:02 -0400

    Updated server

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ const flash = require("connect-flash");
 const LocalStrategy = require("passport-local");
 const PORT = 5000;
 const path = require("path");
+const http = require("http");
 
 const users = require("./routes/user");
 const articles = require("./routes/articles");
@@ -73,7 +74,7 @@ if (process.env.NODE_ENV === "production") {
   //Set static folder
   app.use(express.static("client/build"));
 
-  app.get("*", (req, res) => {
+  app.get("/*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
 }
