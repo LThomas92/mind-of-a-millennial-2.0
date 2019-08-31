cc82cc503927662697f0b97f68aababc85f42fbc
Lawrence Thomas <Lawrence@LawrencsMacBook.home>
2019-08-31 16:33:55 -0400

    Edit Form Update

--- client/src/components/articles/AllArticles.js
+++ client/src/components/articles/AllArticles.js
@@ -32,7 +32,7 @@ class AllArticles extends React.Component {
                 <h3 className="u-center-text">{article.title}</h3>
                 <Link
                   className="article-link"
-                  to={`/api/articles/show/${article.slug}`}
+                  to={`/api/articles/${article.slug}`}
                 >
                   <img
                     className="article-img"
