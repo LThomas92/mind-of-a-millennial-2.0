fd124aa5ae7e6fbc5c497b3982daaec8ee99deaa
Lawrence Thomas <Lawrence@LawrencsMacBook.home>
2019-08-31 20:47:23 -0400

    Add Comment Form

--- client/src/components/comments/CommentItem.js
+++ client/src/components/comments/CommentItem.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import PropTypes from "prop-types";
-import { deleteComment } from "../../actions/articleActions";
+import { deleteComment } from "../actions/articleActions";
 
 class CommentItem extends Component {
   onDeleteClick(articleId, commentId) {
