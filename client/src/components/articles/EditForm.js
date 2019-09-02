e606ec17e46181a3089ecc4e7106697aee9d991f
Lawrence Thomas <Lawrence@LawrencsMacBook.home>
2019-09-02 16:42:50 -0400

    Edit Form

--- client/src/components/articles/EditForm.js
+++ client/src/components/articles/EditForm.js
@@ -77,6 +77,7 @@ class EditForm extends React.Component {
     document.title = "Mind of A Millennial | Edit Article";
 
     const { errors } = this.state;
+    const { article } = this.props.article;
     return (
       <div className="form-container">
         <div className="login">
@@ -94,7 +95,7 @@ class EditForm extends React.Component {
                 <input
                   type="text"
                   placeholder="Enter Article Title"
-                  value={this.state.article.title}
+                  value={article.title}
                   onChange={this.onChangeTitle}
                   error={errors.title}
                   className="form__input"
@@ -112,7 +113,7 @@ class EditForm extends React.Component {
                 <input
                   type="text"
                   placeholder="Enter Image Source"
-                  value={this.state.article.imgSource}
+                  value={article.imgSource}
                   onChange={this.onChangeImgSource}
                   error={errors.image}
                   className="form__input"
@@ -129,7 +130,7 @@ class EditForm extends React.Component {
                   placeholder="Enter Article Category"
                   className="form__input"
                   onChange={this.onChangeCategory}
-                  value={this.state.article.category}
+                  value={article.category}
                   error={errors.category}
                 />
               </div>
@@ -137,7 +138,7 @@ class EditForm extends React.Component {
             <div className="form__group">
               <label className="form__label">Content</label>
               <ReactQuill
-                value={this.state.article.text}
+                value={article.text}
                 error={errors.text}
                 onChange={this.handleChange}
                 modules={EditForm.modules}
