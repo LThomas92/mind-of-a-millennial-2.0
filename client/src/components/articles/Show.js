5337e75f79631999504b1895a6083140afd79b86
Lawrence Thomas <Lawrence@LawrencsMacBook.home>
2019-09-02 16:36:25 -0400

    Edit Form

--- client/src/components/articles/Show.js
+++ client/src/components/articles/Show.js
@@ -1,5 +1,6 @@
 import React from "react";
 import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
 import { connect } from "react-redux";
 import { setCurrentUser } from "../../actions/authActions";
 import { getArticle, deleteArticle } from "../../actions/articleActions";
