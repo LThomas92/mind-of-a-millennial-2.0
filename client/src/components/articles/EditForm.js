cabb48909b162d97efbff7e6488d90c635797d86
Lawrence Thomas <Lawrence@LawrencsMacBook.home>
2019-09-07 14:44:01 -0400

    Edit Form

--- client/src/components/articles/EditForm.js
+++ client/src/components/articles/EditForm.js
@@ -5,6 +5,7 @@ import { withRouter } from "react-router-dom";
 import { editArticle, getArticle } from "../../actions/articleActions";
 import ReactQuill from "react-quill";
 import "react-quill/dist/quill.snow.css";
+import isEmpty from "../../validation/is-empty";
 
 class EditForm extends React.Component {
   state = {
